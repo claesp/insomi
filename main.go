8b083b3fca38cd19a1daae0db9c2a151778f584d
Claes Persson <c@cp.se>
2024-09-03 21:29:02 +0200

    feat: adding go fiber

--- main.go
+++ main.go
@@ -3,12 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 var (
 	Version string = "0.0.0"
+	Port    string = "8080"
 )
 
 func main() {
 	fmt.Fprintf(os.Stdout, "%s\n", Version)
+
+	app := fiber.New()
+
+	app.Get("/", root)
+	app.Listen(fmt.Sprintf(":%s", Port))
+}
+
+func root(c *fiber.Ctx) error {
+	return c.SendString("hello world")
 }
